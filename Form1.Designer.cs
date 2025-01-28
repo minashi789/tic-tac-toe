74213033b2b41eb0412a1f55a641ec768a34cd86
unknown <m2112181@edu.misis.ru>
2025-01-28 13:47:58 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace lab4krestiki
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NewGame = new System.Windows.Forms.Button();
+            this.ResetScore = new System.Windows.Forms.Button();
+            this.Score = new System.Windows.Forms.Label();
+            this.Gamer1 = new System.Windows.Forms.Label();
+            this.Gamer2 = new System.Windows.Forms.Label();
+            this.Score1 = new System.Windows.Forms.Label();
+            this.Score2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.играToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новаяИграToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сброситьСчетToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // NewGame
+            // 
+            this.NewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NewGame.Location = new System.Drawing.Point(88, 560);
+            this.NewGame.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(175, 46);
+            this.NewGame.TabIndex = 0;
+            this.NewGame.Text = "Новая игра";
+            this.NewGame.UseVisualStyleBackColor = true;
+            this.NewGame.Click += new System.EventHandler(this.NewGame_Click);
+            // 
+            // ResetScore
+            // 
+            this.ResetScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ResetScore.Location = new System.Drawing.Point(295, 560);
+            this.ResetScore.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ResetScore.Name = "ResetScore";
+            this.ResetScore.Size = new System.Drawing.Size(175, 46);
+            this.ResetScore.TabIndex = 1;
+            this.ResetScore.Text = "Сбросить счет";
+            this.ResetScore.UseVisualStyleBackColor = true;
+            this.ResetScore.Click += new System.EventHandler(this.ResetScore_Click);
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Score.Location = new System.Drawing.Point(220, 28);
+            this.Score.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(105, 44);
+            this.Score.TabIndex = 2;
+            this.Score.Text = "Счет";
+            // 
+            // Gamer1
+            // 
+            this.Gamer1.AutoSize = true;
+            this.Gamer1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Gamer1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Gamer1.Location = new System.Drawing.Point(57, 24);
+            this.Gamer1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Gamer1.Name = "Gamer1";
+            this.Gamer1.Size = new System.Drawing.Size(78, 24);
+            this.Gamer1.TabIndex = 3;
+            this.Gamer1.Text = "Игрок 1";
+            // 
+            // Gamer2
+            // 
+            this.Gamer2.AutoSize = true;
+            this.Gamer2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Gamer2.Location = new System.Drawing.Point(370, 24);
+            this.Gamer2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Gamer2.Name = "Gamer2";
+            this.Gamer2.Size = new System.Drawing.Size(78, 24);
+            this.Gamer2.TabIndex = 4;
+            this.Gamer2.Text = "Игрок 2";
+            // 
+            // Score1
+            // 
+            this.Score1.AutoSize = true;
+            this.Score1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Score1.Location = new System.Drawing.Point(84, 48);
+            this.Score1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Score1.Name = "Score1";
+            this.Score1.Size = new System.Drawing.Size(20, 24);
+            this.Score1.TabIndex = 5;
+            this.Score1.Text = "0";
+            // 
+            // Score2
+            // 
+            this.Score2.AutoSize = true;
+            this.Score2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Score2.Location = new System.Drawing.Point(395, 48);
+            this.Score2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Score2.Name = "Score2";
+            this.Score2.Size = new System.Drawing.Size(20, 24);
+            this.Score2.TabIndex = 6;
+            this.Score2.Text = "0";
+            this.Score2.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.играToolStripMenuItem,
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(549, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // играToolStripMenuItem
+            // 
+            this.играToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.новаяИграToolStripMenuItem,
+            this.сброситьСчетToolStripMenuItem});
+            this.играToolStripMenuItem.Name = "играToolStripMenuItem";
+            this.играToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.играToolStripMenuItem.Text = "Игра";
+            // 
+            // новаяИграToolStripMenuItem
+            // 
+            this.новаяИграToolStripMenuItem.Name = "новаяИграToolStripMenuItem";
+            this.новаяИграToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.новаяИграToolStripMenuItem.Text = "Новая игра";
+            this.новаяИграToolStripMenuItem.Click += new System.EventHandler(this.новаяИграToolStripMenuItem_Click);
+            // 
+            // сброситьСчетToolStripMenuItem
+            // 
+            this.сброситьСчетToolStripMenuItem.Name = "сброситьСчетToolStripMenuItem";
+            this.сброситьСчетToolStripMenuItem.Size = new System.Drawing.Size(154, 22);
+            this.сброситьСчетToolStripMenuItem.Text = "Сбросить счет";
+            this.сброситьСчетToolStripMenuItem.Click += new System.EventHandler(this.сброситьСчетToolStripMenuItem_Click);
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.загрузитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.загрузитьToolStripMenuItem.Text = "Загрузить";
+            this.загрузитьToolStripMenuItem.Click += new System.EventHandler(this.загрузитьToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 615);
+            this.Controls.Add(this.Score2);
+            this.Controls.Add(this.Score1);
+            this.Controls.Add(this.Gamer2);
+            this.Controls.Add(this.Gamer1);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.ResetScore);
+            this.Controls.Add(this.NewGame);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Игра \"Крестики-нолики\"";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NewGame;
+        private System.Windows.Forms.Button ResetScore;
+        private System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label Gamer1;
+        private System.Windows.Forms.Label Gamer2;
+        private System.Windows.Forms.Label Score1;
+        private System.Windows.Forms.Label Score2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem играToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новаяИграToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сброситьСчетToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab4krestiki
+{
+    public partial class Form1 : Form
+    {
+
+        int x0 = 50, y0 = 80;                           // Координаты начала игрового поля
+        int hod = 0;                                    // Счетчик хода
+        int score1 = 0, score2 = 0;                     // Cчет
+        int[,] table = new int[3, 3];                    // Состояние игрового поля
+        int win = 0;                               // Флаг победы (1-Х, 2-0, 3-ничья)
+        int winx0 = 0, winy0 = 0, winx1 = 0, winy1 = 0; // Координаты выигрышной линии
+        int cur = 0;                                    // Первый ход
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if ((e.Button != MouseButtons.Left) || (e.X < x0) || (e.X > x0 + 450) || (e.Y < y0) || (e.Y > y0 + 450) ||
+                (table[(e.X - x0) / 150, (e.Y - y0) / 150] != 0) || (win != 0)) return;
+
+            if ((hod + cur) % 2 == 0) table[(e.X - x0) / 150, (e.Y - y0) / 150] = 1;
+            else table[(e.X - x0) / 150, (e.Y - y0) / 150] = 2;
+            hod++;
+            Invalidate();               // Перерисовка поля
+            savelog();
+
+            // Проверка выигрыша
+            for (int i = 0; i < 3; i++)
+            {
+                // Горизонтальные линии
+                if (table[i, 0] == table[i, 1] && table[i, 1] == table[i, 2] && table[i, 0] != 0)
+                {
+                    winx0 = 75 + i * 150; winy0 = 10; winx1 = 75 + i * 150; winy1 = 440;
+                    win = table[i, 0];
+                    if (win == 1) score1++; else score2++;
+                    break;
+                }
+                // Вертикальные линии
+                if (table[0, i] == table[1, i] && table[1, i] == table[2, i] && table[0, i] != 0)
+                {
+                    winx0 = 10; winy0 = 75 + i * 150; winx1 = 440; winy1 = 75 + i * 150;
+                    win = table[0, i];
+                    if (win == 1) score1++; else score2++;
+                    break;
+                }
+            }
+            // Диагональные линии
+            if (table[0, 0] == table[1, 1] && table[1, 1] == table[2, 2] && table[0, 0] != 0)
+            {
+                winx0 = 10; winy0 = 10; winx1 = 440; winy1 = 440;
+                win = table[0, 0];
+                if (win == 1) score1++; else score2++;
+            }
+            else if (table[0, 2] == table[1, 1] && table[1, 1] == table[2, 0] && table[0, 2] != 0)
+            {
+                winx0 = 440; winy0 = 10; winx1 = 10; winy1 = 440;
+                win = table[0, 2];
+                if (win == 1) score1++; else score2++;
+            }
+
+            // Проверка на ничью
+            if ((hod == 9) && (win == 0))
+            {
+                MessageBox.Show("Ничья");
+                win = 3;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.TranslateTransform(x0, y0);
+            g.DrawRectangle(new Pen(Color.Black, 3.0f), 0, 0, 450, 450);
+            for (int i = 0; i < 450; i += 150)
+            {
+                g.DrawLine(new Pen(Color.Black, 1.0f), 0, i, 450, i);
+                g.DrawLine(new Pen(Color.Black, 1.0f), i, 0, i, 450);
+            }
+
+            if ((hod+cur) % 2 == 0)
+            {
+                Gamer1.BackColor = Color.BlanchedAlmond;
+                Gamer2.BackColor = Color.White;
+            } else
+            {
+                Gamer2.BackColor = Color.BlanchedAlmond;
+                Gamer1.BackColor = Color.White;
+            }
+
+            for (int i = 0; i < 3; i++) for (int j = 0; j < 3; j++)
+                {
+                    if (table[i, j] == 1)
+                    {
+                        g.DrawLine(new Pen(Color.Red, 5.0f), 10 + i * 150, 10 + j * 150, 140 + i * 150, 140 + j * 150);
+                        g.DrawLine(new Pen(Color.Red, 5.0f), 140 + i * 150, 10 + j * 150, 10 + i * 150, 140 + j * 150);
+                    }
+                    if (table[i, j] == 2) 
+                        g.DrawEllipse(new Pen(Color.Green, 5.0f), 10 + i * 150, 10 + j * 150, 130, 130);
+                }
+            Score1.Text = score1.ToString();
+            Score2.Text = score2.ToString();
+            if (win != 0) g.DrawLine(new Pen(Color.Blue, 8.0f), winx0, winy0, winx1, winy1);
+        }
+
+        void ResetScore1()
+        {
+            score1 = score2 = 0;
+            New();
+            StreamWriter sw = File.CreateText("1.txt");
+            sw.Close();
+        }
+
+        void New()
+        {
+            win = 0;
+            hod = 0;
+            cur = 1 - cur;
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++) table[i, j] = 0;
+            Invalidate();
+        }
+
+        private void NewGame_Click(object sender, EventArgs e)
+        {
+            New();
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (win != 0)
+            {
+                MessageBox.Show("Запись завершенной игры невозможна");
+                return;
+
+            }
+            StreamWriter sw1 = File.CreateText("2.txt");
+
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    sw1.WriteLine(table[i,j].ToString());
+                }
+            }
+            sw1.WriteLine(hod.ToString());
+            sw1.WriteLine(cur.ToString());
+            sw1.WriteLine(score1.ToString());
+            sw1.WriteLine(score2.ToString());
+
+            sw1.Close();
+        }
+
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            StreamReader sr = File.OpenText("2.txt");
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    table[i, j] = Convert.ToInt32(sr.ReadLine());
+                }
+            }
+            hod= Convert.ToInt32(sr.ReadLine());
+            cur = Convert.ToInt32(sr.ReadLine());
+            score1 = Convert.ToInt32(sr.ReadLine());
+            score2 = Convert.ToInt32(sr.ReadLine());
+
+            sr.Close();
+            Invalidate();
+        }
+
+        private void ResetScore_Click(object sender, EventArgs e)
+        {
+            ResetScore1();
+        }
+
+        private void новаяИграToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            New();
+        }
+
+        private void сброситьСчетToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ResetScore1();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void savelog()
+        {
+            StreamWriter sw = File.AppendText("1.txt");
+            string str = "";
+            for (int j = 0; j < 3; j++)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    if (table[i, j] == 0) str += "_ ";
+                    if (table[i, j] == 1) str += "X ";
+                    if (table[i, j] == 2) str += "O ";
+                }
+                str += "\n";
+            }
+            str += "\n";
+            sw.WriteLine(str);
+            sw.Close();
+        }
+
+
+    }
+}
